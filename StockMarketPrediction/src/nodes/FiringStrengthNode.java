2cce43a861a424dfc75fb15b07fdc07df93d1ff9
Steeex <stephan.alaniz@gmail.com>
2013-12-17 14:45:50 +0100

    fixed git merge crash

--- StockMarketPrediction/src/nodes/FiringStrengthNode.java
+++ StockMarketPrediction/src/nodes/FiringStrengthNode.java
@@ -6,24 +6,6 @@ public class FiringStrengthNode extends Node implements NodeClient{
 	
 	public FiringStrengthNode() {
 		super();
-<<<<<<< HEAD
-=======
-		firingStrength = new double[Settings.trainingDataSize];
-	}
-	
-	public double computeFiringStregnth() {
-		incCurrIter();
-		
-		double result;
-		result = ((MembershipFunctionNode)successor.get(0)).getMembershipValue();
-		for(Node n : successor) {
-			if(n instanceof MembershipFunctionNode) {
-				result = firingStrengthFunction(result, ((MembershipFunctionNode)n).getMembershipValue());
-			}
-		}
-		firingStrength[currentIteration] = result;
-		return firingStrength[currentIteration];
->>>>>>> d8fa6ad39ed22c055ce92ff0a978efb10931c509
 	}
 
 	@Override
--- StockMarketPrediction/src/nodes/NormFSNode.java
+++ StockMarketPrediction/src/nodes/NormFSNode.java
@@ -8,22 +8,6 @@ public class NormFSNode extends Node implements NodeClient{
 	public NormFSNode(FiringStrengthNode primaryNode) {
 		super();
 		this.primaryNode = primaryNode;
-
-	public NormFSNode(Node node) {
-		super();
-		this.fsNode = (FiringStrengthNode) node;
-		normalizedFiringStrength = new double[Settings.trainingDataSize];
-	}
-	
-	public static double computeSumOfFiringStrengths(Layer layer) {
-		double result = 0;
-		for(Node n : layer.getNodes()) {
-			if(n instanceof FiringStrengthNode) {
-				result += ((FiringStrengthNode)n).getFiringStrength();
-			}
-		}
-		fsSum = result;
-		return fsSum;
 	}
 
 	@Override

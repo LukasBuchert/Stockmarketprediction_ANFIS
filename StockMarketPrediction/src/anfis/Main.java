d4f1e0e1352d637dee74a1b0208eb7cf4f5155e5
Lukas <Lukas.Buchert@gmx.de>
2014-05-24 14:28:22 +0200

    Adepted Main Class for Gui

--- StockMarketPrediction/src/anfis/Main.java
+++ StockMarketPrediction/src/anfis/Main.java
@@ -5,11 +5,30 @@ import util.*;
 import data.*;
 
 public class Main {
-	public static void main(String[] args) {
 
+	private static String inputPath;
+	private static int numberOfInputVariables;
+	private static int percentage;
+	private static int numberOfShapes;
+	private static int bellSlope;
+	private static int epochNumber;
+	private static String outputPath;
 
-		DataInterface testcase = new DataInterface("ibm_functionforcast_short.csv",3,90,3,2);
+	public Main(String inputPath, int numberOfInputVariables, int percentage,
+			int numberOfShapes, int bellSlope, int epochNumber, String outputPath) {
+		super();
+		this.inputPath = inputPath;
+		this.numberOfInputVariables = numberOfInputVariables;
+		this.percentage = percentage;
+		this.numberOfShapes = numberOfShapes;
+		this.bellSlope = bellSlope;
+		this.epochNumber = epochNumber;
+		this.outputPath = outputPath;
+	}
 
+	public static void runANFIS() {
+		DataInterface testcase = new DataInterface(inputPath,
+				numberOfInputVariables, percentage, numberOfShapes, bellSlope);
 		Settings.numberOfInputVaribles = testcase.getNumberOfInputVariables();
 		Settings.numberOfShapes = testcase.getNumberOfShapes();
 		Settings.bellSlope = testcase.getBellSlope();
@@ -18,38 +37,89 @@ public class Main {
 		ANFIS anfis = new ANFIS();
 		anfis.generateNetwork();
 
-		System.out.println("--generation finished");
-
 		double[][] trainingData = testcase.getTrainigData();
 		double[] expectedOutput = testcase.getExpectedTrainingOutput();
-
-		// Ausfuehren von Anfis
-
 		double[][] result;
-		int epochNumber = 20;
-		double errorRate [][] = new double [epochNumber][1];
-		
-		
-		// = WENN((A2*B2)> 0;1;0)
-		
-		for (int i = 0; i < epochNumber; i++){
+		double errorRate[][] = new double[epochNumber][1];
+
+		for (int i = 0; i < epochNumber; i++) {
 			result = anfis.test(trainingData, expectedOutput);
 			anfis.trainConsequent(trainingData, expectedOutput);
 			anfis.trainPremise(trainingData, expectedOutput);
-			System.out.println("Fehlerrate nach" + i + "-tem Training:" + result[0][0] );
-			errorRate [i][0] = result[0][0];
-			
-		}	
-	
-		result = anfis.test(trainingData, expectedOutput);
-		testcase.writeData(result, "out_ibm_ff_short_20_10000_3m_zc_training.csv");
-		testcase.writeData(errorRate, "out_ibm_ff_short_20_10000_3m_zc_errorrate.csv");
-		
-		result = anfis.test(testcase.getTestData(), testcase.getExpectedTestOutput());
-		
-		
-		testcase.writeData(result, "out_ibm_ff_short_100_10000_3m_zc_test.csv");
-		System.out.println("Finished !!!");
+			errorRate[i][0] = result[0][0];
 
+		}
+
+		result = anfis.test(trainingData, expectedOutput);
+		testcase.writeData(result, outputPath + "_training.csv");
+		testcase.writeData(errorRate, outputPath + "_errorrate.csv");
+
+		result = anfis.test(testcase.getTestData(),
+				testcase.getExpectedTestOutput());
+
+		testcase.writeData(result, outputPath + "_test.csv");
+
+	}
+
+	public static void main(String[] args) {
+
+		inputPath = "ibm_functionforcast_short.csv";
+		numberOfInputVariables = 3;
+		percentage = 90;
+		numberOfShapes = 3;
+		bellSlope = 2;
+		epochNumber = 2;
+		outputPath = "maintest";
+		
+		runANFIS();
+		
+		// DataInterface testcase = new DataInterface(
+		// "ibm_functionforcast_short.csv", 3, 90, 3, 2);
+		//
+		// Settings.numberOfInputVaribles =
+		// testcase.getNumberOfInputVariables();
+		// Settings.numberOfShapes = testcase.getNumberOfShapes();
+		// Settings.bellSlope = testcase.getBellSlope();
+		// Settings.inputVariables = testcase.getInputVariables();
+		//
+		// ANFIS anfis = new ANFIS();
+		// anfis.generateNetwork();
+		//
+		// System.out.println("--generation finished");
+		//
+		// double[][] trainingData = testcase.getTrainigData();
+		// double[] expectedOutput = testcase.getExpectedTrainingOutput();
+		//
+		// // Ausfuehren von Anfis
+		//
+		// double[][] result;
+		// // int epochNumber = 20;
+		// double errorRate[][] = new double[epochNumber][1];
+		//
+		// // = WENN((A2*B2)> 0;1;0)
+		//
+		// for (int i = 0; i < epochNumber; i++) {
+		// result = anfis.test(trainingData, expectedOutput);
+		// anfis.trainConsequent(trainingData, expectedOutput);
+		// anfis.trainPremise(trainingData, expectedOutput);
+		// System.out.println("Fehlerrate nach" + i + "-tem Training:"
+		// + result[0][0]);
+		// errorRate[i][0] = result[0][0];
+		//
+		// }
+		//
+		// result = anfis.test(trainingData, expectedOutput);
+		// testcase.writeData(result,
+		// "out_ibm_ff_short_20_10000_3m_zc_training.csv");
+		// testcase.writeData(errorRate,
+		// "out_ibm_ff_short_20_10000_3m_zc_errorrate.csv");
+		//
+		// result = anfis.test(testcase.getTestData(),
+		// testcase.getExpectedTestOutput());
+		//
+		// testcase.writeData(result,
+		// "out_ibm_ff_short_100_10000_3m_zc_test.csv");
+		// System.out.println("Finished !!!");
+		//
 	}
 }
